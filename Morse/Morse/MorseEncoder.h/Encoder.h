4ac2770e772c9bac7aec0215f9e1dc0258ba17bb
Valerii <150512618+vasavass@users.noreply.github.com>
2025-04-12 09:32:02 +0300

    Add files via upload

---
+++ Morse/Morse/MorseEncoder.h/Encoder.h
@@ -0,0 +1,13 @@
+#ifndef ENCODER_H
+#define ENCODER_H
+
+#include <string>
+
+class Encoder {
+public:
+    virtual ~Encoder() = default;
+    virtual std::string encode(const std::string& input) const = 0;
+    virtual std::string decode(const std::string& input) const = 0;
+};
+
+#endif // ENCODER_H
